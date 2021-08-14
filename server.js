08b38398a4dfc2a63e3d9a5cf6f854174b90dda2
Yoga Adi Saputra <syogaadi75@gmail.com>
2021-08-14 13:18:48 +0700

    membenarkan bug

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+// panggil routes
+var routes = require('./routes');
+routes(app)
+
 // Parse application/json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
